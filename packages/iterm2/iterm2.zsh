b6bc71cd4ad9f81dc09df9bca1719eead7496e62
Johan Borestad <johan@borestad.com>
2016-05-19 17:30:45 +0200

    Improve colors in iterm2

--- packages/iterm2/iterm2.zsh
+++ packages/iterm2/iterm2.zsh
@@ -5,6 +5,8 @@
 # iTerm2 window/tab color commands
 #   Requires iTerm2 >= Build 1.0.0.20110804
 #   http://code.google.com/p/iterm2/wiki/ProprietaryEscapeCodes
+source $DOTJITSU/packages/iterm2/iterm2_helpers.sh
+
 tab-color() {
     echo -ne "\033]6;1;bg;red;brightness;$1\a"
     echo -ne "\033]6;1;bg;green;brightness;$2\a"
@@ -20,9 +22,9 @@ color-ssh() {
     if [[ -n "$ITERM_SESSION_ID" ]]; then
         trap "tab-reset" INT EXIT
         if [[ "$*" =~ "production|ec2-.*compute-1" ]]; then
-            tab-color 255 0 0
+            tab_pure_red
         else
-            tab-color 0 255 0
+            tab_lime_green
         fi
     fi
     ssh $*
