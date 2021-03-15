27c14fcc9202be67697af79e5dfe6a44dc337dc8
Johan Borestad <johan.borestad@teliacompany.com>
2021-03-15 19:14:18 +0100

    bump

--- packages/.hammerspoon/MiddleMouseScroll.lua
+++ packages/.hammerspoon/MiddleMouseScroll.lua
@@ -5,8 +5,8 @@ local deferred = false
 overrideOtherMouseDown = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDown }, function(e)
     -- print("down")
     local pressedMouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
-    if scrollMouseButton == pressedMouseButton 
-        then 
+    if scrollMouseButton == pressedMouseButton
+        then
             deferred = true
             return true
         end
@@ -15,8 +15,8 @@ end)
 overrideOtherMouseUp = hs.eventtap.new({ hs.eventtap.event.types.otherMouseUp }, function(e)
     -- print("up")
     local pressedMouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
-    if scrollMouseButton == pressedMouseButton 
-        then 
+    if scrollMouseButton == pressedMouseButton
+        then
             if (deferred) then
                 overrideOtherMouseDown:stop()
                 overrideOtherMouseUp:stop()
@@ -36,20 +36,20 @@ local scrollmult = -4   -- negative multiplier makes mouse work like traditional
 dragOtherToScroll = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDragged }, function(e)
     local pressedMouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
     -- print ("pressed mouse " .. pressedMouseButton)
-    if scrollMouseButton == pressedMouseButton 
-        then 
+    if scrollMouseButton == pressedMouseButton
+        then
             -- print("scroll");
             deferred = false
-            oldmousepos = hs.mouse.getAbsolutePosition()    
+            oldmousepos = hs.mouse.getAbsolutePosition()
             local dx = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaX'])
             local dy = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaY'])
             local scroll = hs.eventtap.event.newScrollEvent({-dx * scrollmult, dy * scrollmult},{},'pixel')
             -- put the mouse back
-            hs.mouse.setAbsolutePosition(oldmousepos)
+            hs.mouse.absolutePosition(oldmousepos)
             return true, {scroll}
-        else 
+        else
             return false, {}
-        end 
+        end
 end)
 
 overrideOtherMouseDown:start()
